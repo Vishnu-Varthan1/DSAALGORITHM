f4d69681f88d28dbcb3aeffe84b6b319d1132eed
vishnuvarthan <166686421+Vishnu-Varthan1@users.noreply.github.com>
2025-10-04 09:21:36 +0530

    Update class_object.cpp

--- OOPS/class_object.cpp
+++ OOPS/class_object.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    void greet() {
+        cout << "Hello from MyClass!" << endl;
+    }
+};
+
+int main() {
+    MyClass obj;
+    obj.greet();
+    return 0;
+}
 
